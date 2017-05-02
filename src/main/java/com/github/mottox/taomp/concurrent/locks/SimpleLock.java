aa04acfe9aca50979d8031541b749cae00bdb8c9
MottoX <micrari@gmail.com>
2017-05-02 12:19:29 +0800

    Implement SimpleReadWriteLock.java

--- src/main/java/com/github/mottox/taomp/concurrent/locks/SimpleLock.java
+++ src/main/java/com/github/mottox/taomp/concurrent/locks/SimpleLock.java
@@ -1,9 +1,13 @@
 package com.github.mottox.taomp.concurrent.locks;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 /**
  * TAOMP中简化版的{@link java.util.concurrent.locks.Lock}。
  */
-public interface SimpleLock {
+public interface SimpleLock extends Lock {
 
     /**
      * 在进入临界区之前调用。
@@ -15,4 +19,23 @@ public interface SimpleLock {
      */
     void unlock();
 
+    @Override
+    default void lockInterruptibly() throws InterruptedException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default boolean tryLock() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    default Condition newCondition() {
+        throw new UnsupportedOperationException();
+    }
 }
